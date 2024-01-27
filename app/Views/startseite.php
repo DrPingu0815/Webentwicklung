d39a271746e3eff0e3ce4297fb7fe8a3cda2f138
s4niseib <s4niseib@uni-trier.de>
2024-01-27 17:06:39 +0100

    Übung 7 Aufgabe 1 fertig. Aufgabe 2 fehlt

--- app/Views/startseite.php
+++ app/Views/startseite.php
@@ -8,7 +8,7 @@ use App\Controllers\Tasks;
 ?>
 
 <div class="container-fluid">
-            <div class="card">
+            <div class="card mb-3 ">
                 <div class="card-header d-flex justify-content-between align-items-center">
                     <h3 class="card-title">Tasks</h3>
                     <a class="btn btn-primary btn-sm" href="<?= base_url('Tasks/TaskCRUD') ?>" role="button">Neu</a>
@@ -29,16 +29,86 @@ use App\Controllers\Tasks;
                                         <?php foreach ($tasks as $task): ?>
                                             <?php if ($task['spalte'] == $item['spalte']): ?>
                                         <div class="card mb-2">
+                                            <div class="container-fluid">
 
-                                                <p><strong>TaskID:</strong> <?= $task['id'] ?></p>
-                                                <p><strong>Vorname:</strong> <?= $task['vorname'] ?></p>
-                                                <p><strong>Nachname:</strong> <?= $task['name'] ?></p>
-                                                <p><strong>Taskart:</strong> <?= $task['taskart'] ?></p>
-                                                <p><strong>SortID:</strong> <?= $task['sortid'] ?></p>
-                                                <p><strong>Task:</strong> <?= $task['tasks'] ?></p>
-                                                <p><strong>Erstellungsdatum:</strong> <?= $task['erstellungsdatum'] ?></p>
-                                                <p><strong>Erinnerungsdatum:</strong> <?= $task['erinnerungsdatum'] ?></p>
+                                                <!-- Taskartenicon, und Taskbezeichnung inklusive Aktionen zugehörig zu der Task -->
 
+                                                <div class="d-flex justify-content-between mb-1">
+                                                    <a href="<?= base_url('Tasks/TaskCRUD/' . $task['id'].'/'.'1') ?>" class="text-decoration-none">
+                                                        <p class="ms-2 mt-3"><?= $task['taskartenicon'] ?> <?= $task['tasks'] ?></p>
+                                                    </a>
+
+
+
+                                                        <span class="dropdown position-static mt-3">
+                                                    <a class="btn btn-link ps-0 pt-0 pb-0 pe-2" role="button" data-bs-toggle="dropdown" data-boundary="viewport" aria-haspopup="true" aria-expanded="false" >
+                                                        <i class="fas fa-caret-square-down text-primary"></i>
+                                                    </a>
+                                                        <div class="dropdown-menu">
+                                                            <li>
+                                                            <p class="dropdown-item mb-0"><strong>Aktionen</strong></p>
+                                                            </li>
+                                                            <li class="dropdown-divider"></li>
+                                                            <li>
+                                                                  <a id='btnBearbeiten' class="dropdown-item" href="<?= base_url('Tasks/TaskCRUD/' . $task['id'].'/'.'1') ?>">
+                                                                      <span title="Task bearbeiten" class="icon-menu text-primary"><i class="fas fa-edit"></i></span>
+                                                                            Task bearbeiten
+                                                                  </a>
+                                                            </li>
+
+                                                            <li>
+                                                                <a id='btnLoeschen' class="dropdown-item" href="<?= base_url('Tasks/TaskCRUD/' . $task['id'] . '/' . 2) ?>">
+                                                                     <span title="Task löschen" class="icon-menu text-primary"><i class="fa-solid fa-trash"></i></span>
+                                                                        Task löschen
+                                                                </a>
+
+                                                            </li>
+
+
+                                                        </div>
+                                                    </span>
+                                                </div>
+
+
+
+                                                <!-- Erstellungsdatum -->
+
+                                                <div class="mb-1 d-flex justify-content-between">
+                                                    <div class="text-secondary">
+                                                        <p class="ms-2">
+                                                            <i class="fa-solid fa-calendar me-1"></i>
+                                                            <?= $task['erstellungsdatum'] ?>
+                                                        </p>
+                                                    </div>
+                                                    <div class="me-2 text-secondary">
+
+                                                        <p class="d-flex align-items-center ms-2">
+                                                            <i class="fa-regular fa-bell me-1" style="color: red;"></i>
+                                                            <?= $task['erinnerungsdatum'] ?>
+                                                        </p>
+                                                    </div>
+                                                </div>
+
+
+                                                <!-- Name und Vorname der Person -->
+
+                                                <div class="mb-1">
+
+                                                    <p class="ms-2 text-secondary">
+                                                     <i class="fa-regular fa-user me-1"></i>
+                                                     <?= $task['vorname'] ?>
+                                                     <?= $task['name'] ?>
+                                                    </p>
+                                                </div>
+
+
+
+
+
+
+
+
+                                        </div>
                                         </div>
 
                                             <?php endif; ?>
