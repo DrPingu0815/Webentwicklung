81508bd74eba7fac3219d76451cf7b4fbd9015bb
PinguProgrammer <molitorsven@hotmail.de>
2024-02-05 13:29:57 +0100

    Menu Bar Gleiche Größe

--- app/Views/templates/menu.php
+++ app/Views/templates/menu.php
@@ -1,28 +1,26 @@
 
 
 <!-- Tasks Menu -->
-<nav class="navbar navbar-expand-md navbar-dark bg-header-1 bg-header-2 px-0 py-0 pb-1 mb-3" role="navigation">
-    <div class="container justify-content-start">
-        <div id="navbarContent" class="collapse navbar-collapse ms-2 ms-md-0">
-            <ul class="navbar-nav me-auto ms-2 ms-md-0">
-                <li class="nav-item active">
-                    <a class="nav-link text-white py-0 mt-1" href="<?= base_url('Tasks/Startseite') ?>">
+<nav class=" navbar navbar-expand-lg navbar-dark bg-header-1 bg-header-2 px-0 py-0 mb-2 flex-grow-0 " role="navigation">
+        <div id="navbarContent" class="collapse navbar-collapse ms-2 ms-md-0 p-2 justify-content ">
+            <ul class="navbar-nav me-auto ms-2 ms-md-0 d-flex gap-2">
+                <li class="nav-item">
+                    <a class="nav-link text-white py-0 " href="<?= base_url('Tasks/Startseite') ?>">
                         <i class="fas fas fa-solid fa-clipboard-check text-sx icon-main-menu"></i><span class="d-inline-flex d-md-none d-lg-none d-xl-inline-flex">Tasks</span>
                     </a>
                 </li>
 
-                <li class="nav-item active">
-                    <a class="nav-link text-white py-0 mt-1" href="<?= base_url('Boards/Boardsseite') ?>">
+                <li class="nav-item">
+                    <a class="nav-link text-white py-0" href="<?= base_url('Boards/Boardsseite') ?>">
                         <i class="fas fas fa-solid fa-clipboard-check text-sx icon-main-menu"></i><span class="d-inline-flex d-md-none d-lg-none d-xl-inline-flex">Boards</span>
                     </a>
                 </li>
 
-                <li class="nav-item active">
-                    <a class="nav-link text-white py-0 mt-1" href="<?= base_url('Spalten/Spaltenseite') ?>">
-                        <i class="fas fa-solid fa-table-columns text-sx icon-main-menu"></i><span class="d-inline-flex d-md-none d-lg-none d-xl-inline-flex">Spalten</span>
+                <li class="nav-item">
+                    <a class="nav-link text-white py-0" href="<?= base_url('Spalten/Spaltenseite') ?>">
+                        <i class="fas fa-solid fa-table-columns text-sx icon-main-menu"></i><span class="d-inline-flex d-md-none d-lg-none d-xl-inline-flex ml-1 ">Spalten</span>
                     </a>
                 </li>
             </ul>
         </div>
-    </div>
 </nav>
