d53d95d7b31bf7ecf73ea6b563d2af7c6aaf3c15
PinguProgrammer <molitorsven@hotmail.de>
2024-02-05 14:58:22 +0100

    MenuBurger erstellt
    Wechselt auf Burger sobald die Größe Medium oder kleiner wird.

--- app/Views/templates/menu.php
+++ app/Views/templates/menu.php
@@ -1,26 +1,35 @@
 
 
 <!-- Tasks Menu -->
-<nav class=" navbar navbar-expand-lg navbar-dark bg-header-1 bg-header-2 px-0 py-0 mb-2 flex-grow-0 " role="navigation">
-        <div id="navbarContent" class="collapse navbar-collapse ms-2 ms-md-0 p-2 justify-content ">
-            <ul class="navbar-nav me-auto ms-2 ms-md-0 d-flex gap-2">
+<nav class=" navbar navbar-expand-lg navbar-dark bg-header-1 bg-header-2 px-0 py-1 mb-2  flex-grow-0 " role="navigation">
+    <div class="container-fluid">
+        <button class="navbar-toggler ms-2 ms-md-0 d-block d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+                aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="nav-link ps-0 pe-0" href="#">
+                <i class="fas fa-bars"></i>
+                <span class="text-white"></span>
+            </a>
+        </button>
+        <div id="navbarContent" class="collapse navbar-collapse  d-lg-none ms-2 ms-md-0 p-2 ">
+            <ul class="navbar-nav mr-auto ms-2 ms-md-0 gap-2">
                 <li class="nav-item">
                     <a class="nav-link text-white py-0 " href="<?= base_url('Tasks/Startseite') ?>">
-                        <i class="fas fas fa-solid fa-clipboard-check text-sx icon-main-menu"></i><span class="d-inline-flex d-md-none d-lg-none d-xl-inline-flex">Tasks</span>
+                        <i class="fas fas fa-solid fa-clipboard-check text-sx icon-main-menu"></i><span class="">Tasks</span>
                     </a>
                 </li>
 
                 <li class="nav-item">
                     <a class="nav-link text-white py-0" href="<?= base_url('Boards/Boardsseite') ?>">
-                        <i class="fas fas fa-solid fa-clipboard-check text-sx icon-main-menu"></i><span class="d-inline-flex d-md-none d-lg-none d-xl-inline-flex">Boards</span>
+                        <i class="fas fas fa-solid fa-clipboard-check text-sx icon-main-menu"></i><span class="">Boards</span>
                     </a>
                 </li>
 
                 <li class="nav-item">
                     <a class="nav-link text-white py-0" href="<?= base_url('Spalten/Spaltenseite') ?>">
-                        <i class="fas fa-solid fa-table-columns text-sx icon-main-menu"></i><span class="d-inline-flex d-md-none d-lg-none d-xl-inline-flex ml-1 ">Spalten</span>
+                        <i class="fas fa-solid fa-table-columns text-sx icon-main-menu"></i><span class="">Spalten</span>
                     </a>
                 </li>
             </ul>
         </div>
+    </div>
 </nav>
